d5e31780eb580cae3043214737527663c84b2031
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2026-04-06 08:18:21 +0800

    refactor: consolidate .visually-hidden to global CSS

--- src/routes/lists/[id]/+page.svelte
+++ src/routes/lists/[id]/+page.svelte
@@ -667,15 +667,4 @@
     box-shadow: none;
   }
 
-  .visually-hidden {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    white-space: nowrap;
-    border: 0;
-  }
 </style>
